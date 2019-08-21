f06494bfa30544d624c8d85cb12f5380fa9f02c2
Sarah Guarino <guarino.sgg@gmail.com>
2019-08-21 05:39:18 -0400

    Royal Pop of 3 returns Generation 0 Pop of 3

--- src/MergeCalculator.java
+++ src/MergeCalculator.java
@@ -1,7 +1,9 @@
 class MergeCalculator {
 
-    int[] getTargetPopByGen(int specifiedGen) {
+    int[] getTargetPopByGen(int specifiedGen, int royalPop) {
         int[] targetPopByGen = new int[specifiedGen];
+
+        targetPopByGen[0] = royalPop;
 
         return targetPopByGen;
     }
--- test/MergeCalculatorTest.java
+++ test/MergeCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class MergeCalculatorTest {
     private MergeCalculator calculator;
     private int specifiedGen;
+    private int royalPop;
     private int[] targetPopByGen;
 
     public MergeCalculatorTest() {
@@ -13,11 +14,21 @@ public class MergeCalculatorTest {
     @Test
     public void specifiedGenOf2ReturnsGenLength2() {
         specifiedGen = 1;
+        royalPop = 3;
 
-        targetPopByGen = calculator.getTargetPopByGen(specifiedGen);
+        targetPopByGen = calculator.getTargetPopByGen(specifiedGen, royalPop);
 
         Assert.assertEquals(targetPopByGen.length, specifiedGen);
     }
 
+    @Test
+    public void royalPopOf3ReturnsGen0PopOf3() {
+        specifiedGen = 1;
+        royalPop = 3;
+
+        targetPopByGen = calculator.getTargetPopByGen(specifiedGen, royalPop);
+
+        Assert.assertEquals(targetPopByGen[0], 3);
+    }
 
 }

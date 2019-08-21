6b93b85c7db11f4ea401aac5b19e20e9f6a4c5cf
Sarah Guarino <guarino.sgg@gmail.com>
2019-08-21 14:30:37 -0400

    Removed some constructor variables.

--- test/MergeCalculatorTest.java
+++ test/MergeCalculatorTest.java
@@ -3,48 +3,39 @@ import org.junit.Test;
 
 public class MergeCalculatorTest {
     private MergeCalculator calculator;
-    private int specifiedGen;
-    private int royalPop;
     private int[] targetPopByGen;
-    private int[] currentPopByGen;
 
     public MergeCalculatorTest() {
         calculator = new MergeCalculator();
-        specifiedGen = 1;
-        royalPop = 3;
-        currentPopByGen = new int[]{0};
     }
 
     @Test
     public void specifiedGenOf2ReturnsGenLength2() {
-        targetPopByGen = calculator.getTargetPopByGen(specifiedGen, royalPop, currentPopByGen);
+        targetPopByGen = calculator.getTargetPopByGen(2, 3, new int[]{0});
 
-        Assert.assertEquals(targetPopByGen.length, specifiedGen);
+        Assert.assertEquals(targetPopByGen.length, 2);
     }
 
     @Test
     public void royalPopOf3ReturnsGen0PopOf3() {
-        targetPopByGen = calculator.getTargetPopByGen(specifiedGen, royalPop, currentPopByGen);
+        targetPopByGen = calculator.getTargetPopByGen(1, 3, new int[]{0});
 
         Assert.assertEquals(targetPopByGen[0], 3);
     }
 
     @Test
     public void royalPopOf3And1OwnedReturnsGen0PopOf2() {
-        currentPopByGen = new int[]{1};
-
-        targetPopByGen = calculator.getTargetPopByGen(specifiedGen,royalPop, currentPopByGen);
+        targetPopByGen = calculator.getTargetPopByGen(1,3, new int[]{1});
 
         Assert.assertEquals(targetPopByGen[0], 2);
     }
 
     @Test
     public void royalPopOf3ReturnsGen1PopOf10() {
-        specifiedGen = 2;
-
-        targetPopByGen = calculator.getTargetPopByGen(specifiedGen,royalPop,currentPopByGen);
+        targetPopByGen = calculator.getTargetPopByGen(2,3, new int[]{0});
 
         Assert.assertEquals(targetPopByGen[1], 10);
-
     }
+
+
 }

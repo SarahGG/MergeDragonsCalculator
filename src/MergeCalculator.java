a898375e8a6616b4177651da4ae7f5e179be43b5
Sarah Guarino <guarino.sgg@gmail.com>
2019-08-21 14:31:35 -0400

    Parsed out method for calculating parents needed for the lg sized merges

--- src/MergeCalculator.java
+++ src/MergeCalculator.java
@@ -1,4 +1,11 @@
 class MergeCalculator {
+    private int numChildrenPerLgMerge;
+    private int numParentPerLgMerge;
+
+    public MergeCalculator() {
+        numChildrenPerLgMerge = 3;
+        numParentPerLgMerge = 10;
+    }
 
     int[] getTargetPopByGen(int specifiedGen, int royalPop, int[] currentPopByGen) {
         int[] targetPopByGen = new int[specifiedGen];
@@ -7,14 +14,23 @@ class MergeCalculator {
             if (i == 0) {
                 targetPopByGen[i] = royalPop - currentPopByGen[i];
             } else {
-                targetPopByGen[i] = getTotalParentsNeeded();
+                targetPopByGen[i] = getTotalParentsNeeded(targetPopByGen[i-1]);
             }
         }
 
         return targetPopByGen;
     }
 
-    private int getTotalParentsNeeded () {
-        return 10;
+    private int getTotalParentsNeeded (int childrenNeeded) {
+        int totalParentsNeeded = 0;
+
+        totalParentsNeeded += getTotalParentsNeededForLgMerges(childrenNeeded);
+        childrenNeeded = childrenNeeded % numChildrenPerLgMerge;
+
+        return totalParentsNeeded;
+    }
+
+    private int getTotalParentsNeededForLgMerges(int childrenNeeded) {
+        return (childrenNeeded / numChildrenPerLgMerge) * numParentPerLgMerge;
     }
 }
